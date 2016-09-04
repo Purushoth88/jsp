b163ffdb525fff17f53c55d9b69b4d4bd1b6e006
Robert Rong <robert.rong@agile-solutions.ch>
2016-09-04 12:08:10 +0200

    another one

--- src/ch/agilesolutions/jsp/test/UnzipTest.java
+++ src/ch/agilesolutions/jsp/test/UnzipTest.java
@@ -11,31 +11,38 @@ import java.util.zip.ZipFile;
 
 import org.apache.commons.io.IOUtils;
 
+/**
+ * http://www.baeldung.com/java-compress-and-uncompress
+ * 
+ * http://stackoverflow.com/questions/9324933/what-is-a-good-java-library-to-zip-unzip-files
+ * 
+ * @author
+ *
+ */
 public class UnzipTest {
 
 	public static void main(String[] args) throws IOException {
 
-		
-		
-		java.util.zip.ZipFile zipFile = new ZipFile("C:\\Development\\Workspaces\\JSP\\rob\\lib\\commons-io-2.5-bin.zip");
+		java.util.zip.ZipFile zipFile = new ZipFile(
+				"C:\\Development\\Workspaces\\JSP\\rob\\lib\\commons-io-2.5-bin.zip");
 		try {
-		  Enumeration<? extends ZipEntry> entries = zipFile.entries();
-		  while (entries.hasMoreElements()) {
-		    ZipEntry entry = entries.nextElement();
-		    File entryDestination = new File("C:\\Development\\Workspaces\\JSP\\rob\\tmp",  entry.getName());
-		    if (entry.isDirectory()) {
-		        entryDestination.mkdirs();
-		    } else {
-		        entryDestination.getParentFile().mkdirs();
-		        InputStream in = zipFile.getInputStream(entry);
-		        OutputStream out = new FileOutputStream(entryDestination);
-		        IOUtils.copy(in, out);
-		        IOUtils.closeQuietly(in);
-		        out.close();
-		    }
-		  }
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				ZipEntry entry = entries.nextElement();
+				File entryDestination = new File("C:\\Development\\Workspaces\\JSP\\rob\\tmp", entry.getName());
+				if (entry.isDirectory()) {
+					entryDestination.mkdirs();
+				} else {
+					entryDestination.getParentFile().mkdirs();
+					InputStream in = zipFile.getInputStream(entry);
+					OutputStream out = new FileOutputStream(entryDestination);
+					IOUtils.copy(in, out);
+					IOUtils.closeQuietly(in);
+					out.close();
+				}
+			}
 		} finally {
-		  zipFile.close();
+			zipFile.close();
 		}
 	}
 }
